ea81aa824e417b344836568b7280bf7de5ff372a
murodjon72 <muro21601@gmail.com>
2024-07-17 21:23:22 +0500

    some updates

--- script.js
+++ script.js
@@ -21,8 +21,8 @@ console.log(totalBalance);
 
 let str  =  'aaa@bbb@ccc' 
 
-str.replaceAll('@', '!');
-console.log(str);
+
+console.log(str.split('@').join('!'));
 
 let str2 = "HELLOWORLD"
 
@@ -32,7 +32,8 @@ console.log(upper)
 let html ='Hello, it is HTML'
 let js =' not JS'
 
-
+let randomNumber = parseInt(Math.random() * 100);
+console.log(randomNumber);
 
 console.log( html.slice(0, 12) + js);
 
@@ -42,67 +43,14 @@ let lower = str3.slice(0, 1).toUpperCase() + str3.slice(1).toLowerCase();
 console.log(lower)
 
 let lorem = 'Lex Luterl has A big suit';
-let a = lorem.indexOf('A');
+let a = lorem.indexOf('a');
 let l = lorem.indexOf('l');
 let e = lorem.indexOf('e');
 let x = lorem.indexOf('x');
 
 let alex = lorem[a] + lorem[l] +lorem[e] + lorem[x];
+
+alex = alex.splice(0, 1).toUpperCase() + alex.splice(1).toLowerCase();
 console.log(alex);
-
-
-const amount = 10000;
-const account = 7777;
-const username = 'Alex';
-
-const names = prompt('What\'s your name');
-
-if (name === username) {
- const accountNumber = +prompt('What\'s your account number');
-
- if (accountNumber === account) {
- const amountMoney = +prompt('How much you want get');
-
-if (amountMoney < amount) {
- alert(`cashed out: ${amountMoney}, left: ${amount - amountMoney}`);} else {
-alert('Invalid amount of money'); }} 
-else {alert('No') }}
-  else {alert('No')}
-
-  const cars = [
-    { make: 'Toyota', model: 'Camry', price: 30000 },
-    { make: 'Ford', model: 'Mustang', price: 50000 },
-    { make: 'Honda', model: 'Civic', price: 22000 },
-    { make: 'BMW', model: 'X5', price: 55000 },
-    { make: 'Hyundai', model: 'Elantra', price: 27000 },
-    { make: 'Chevrolet', model: 'Malibu', price: 32000 }
-  ];
-  const cheap = cars.filter(car => car.price < 35000);
-
-  console.log(cheap);
-
-  let arr = [1, 2, false, 'hello', 24, 'world', undefined, null, 'error', 22];
-
-
-let numbers = arr.filter(item => typeof item === 'number');
-
-
-if (numbers.length > 5) {
-}
-  console.log('Гуд');
-
-  let index = prompt();
-  let array = ['Aleksey', 'Kartoshka', 'Margarita' , 'Morgenshtern', 'Monica'];
-  if(index < arr.length){
-    array.splice(index, 1);
-  }
-  else{
-    alert('index undefined')
-  }
-
-
-
-
-
 
 
